a72e59e8087274725fb183e16747a1855f24f498
David Dodel <david.dodel@hotmail.com>
2018-10-09 23:46:37 +0200

    Added comments for last touch ups

--- src/kalman/kalman_tracker.h
+++ src/kalman/kalman_tracker.h
@@ -9,5 +9,30 @@
 
 template <typename T>
 
+// Class to wrap a kalman tracker
+// Map obj_ids to instances of kfs coming from a kf factory (lol!) -> Just have the transition matrix etc. as private members here...
+// All have the same transition matrix, meas_mtx etc.) but different starting states
+/*
+ * State x := (x, y, dx, dy)^T
+ * Measurements z := (x, y)^T
+ *
+ * Goal of KF is to smooth the trend of the bboxes
+ */
+
+/*
+ * Alternativ:
+ *
+ * State x := (h, v, a)^T
+ * Measurement z := (h, v, a)^T
+ *
+ */
+
+/*
+ * Alternativ: KFs for smoothing and for predicting the velocity -> v from vis. od. fused with accell from IMU
+ *
+ * State x := (v, a)^T
+ * Measurement z := (v, a)^T
+ *
+ */
 
 #endif //WRAPPER_KALMAN_TRACKER_H
