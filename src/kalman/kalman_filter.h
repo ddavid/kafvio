e121841f887778371c6e11fa01d8186dd28b8934
David Dodel <david.dodel@hotmail.com>
2018-10-10 14:02:06 +0200

    Added defaults and corrected some function signatures

--- src/kalman/kalman_filter.h
+++ src/kalman/kalman_filter.h
@@ -13,6 +13,9 @@ namespace cpppc
   template<int StateDim, int MeasDim, int CtlDim, typename T>
   class Kalman_Filter {
 
+    typedef Kalman_Filter<StateDim, MeasDim, CtlDim, T>  self_t;
+    typedef T                                            value_t;
+
   public:
     Kalman_Filter() :
           pre_state(pre_state.Zero())
@@ -28,46 +31,58 @@ namespace cpppc
         , residual(residual.Zero())
         {};
 
-    Kalman_Filter(const Eigen::Matrix<T, StateDim, StateDim> transition_matrix
-                , const Eigen::Matrix<T, MeasDim, StateDim> measurement_matrix  = Eigen::Matrix<T, MeasDim, StateDim>::Zero()
-                , const Eigen::Matrix<T, StateDim, CtlDim> control_matrix       = Eigen::Matrix<T, StateDim, CtlDim>::Zero())
-                : Kalman_Filter()
+    Kalman_Filter(const Eigen::Matrix<value_t, StateDim, StateDim> transition_matrix
+        , const Eigen::Matrix<value_t, MeasDim, StateDim> measurement_matrix  = Eigen::Matrix<value_t, MeasDim, StateDim>::Zero()
+        , const Eigen::Matrix<value_t, StateDim, CtlDim> control_matrix       = Eigen::Matrix<value_t, StateDim, CtlDim>::Zero())
+        : Kalman_Filter()
     {
       this->set_transition_mtx(transition_matrix);
       this->set_meas_mtx(measurement_matrix);
       this->set_ctl_mtx(control_matrix);
     }
 
+    // No ownership
+    // |
+    // -> defaults for Destructor, Copy/Move Constructor, and for Copy/Move Constructor/Assignment
+
+    ~Kalman_Filter()                          = default;
+    Kalman_Filter(const self_t &)             = default;
+    Kalman_Filter(self_t &&)                  = default;
+    self_t & operator=(const self_t &)        = default;
+    self_t & operator=(self_t &&)             = default;
+
+
+
     // Normal KF, but possibility to extend to EKF
 
-    void set_transition_mtx( const Eigen::Matrix<T, StateDim, StateDim> transition_mtx ) {
+    void set_transition_mtx( const Eigen::Matrix<value_t, StateDim, StateDim> transition_mtx ) {
       this->transition_mtx = transition_mtx;
     };
 
-    void set_ctl_mtx( const Eigen::Matrix<T, StateDim, CtlDim> ctl_mtx ) {
+    void set_ctl_mtx( const Eigen::Matrix<value_t, StateDim, CtlDim> ctl_mtx ) {
       this->ctl_mtx = ctl_mtx;
     };
 
-    void set_meas_mtx( const Eigen::Matrix<T, MeasDim, StateDim> meas_mtx ) {
+    void set_meas_mtx( const Eigen::Matrix<value_t, MeasDim, StateDim> meas_mtx ) {
       this->meas_mtx = meas_mtx;
     };
 
-    void set_process_noise(const Eigen::Matrix<float, StateDim, StateDim> process_noise)
+    void set_process_noise(const Eigen::Matrix<value_t, StateDim, StateDim> process_noise)
     {
       this->process_noise = process_noise;
     };
 
-    void set_process_cov(const Eigen::Matrix<float, StateDim, StateDim> post_process_cov)
+    void set_process_cov(const Eigen::Matrix<value_t, StateDim, StateDim> post_process_cov)
     {
       this->post_process_cov = post_process_cov;
     };
 
-    void set_measurement_noise(const Eigen::Matrix<float, MeasDim, MeasDim> meas_noise)
+    void set_measurement_noise(const Eigen::Matrix<value_t, MeasDim, MeasDim> meas_noise)
     {
       this->meas_noise = meas_noise;
     };
 
-    void predict( const Eigen::Matrix<T, CtlDim, 1> ctl_vec = Eigen::Matrix<T, CtlDim, 1>::Zero()) {
+    void predict( const Eigen::Matrix<value_t, CtlDim, 1> ctl_vec = Eigen::Matrix<value_t, CtlDim, 1>::Zero()) {
       // Use process model to predict state at the next time step
       // x' = Fx [+ Bu]
       pre_state = transition_mtx * post_state;
@@ -81,7 +96,7 @@ namespace cpppc
       pre_process_cov = transition_mtx * post_process_cov * transition_mtx.transpose() + process_noise;
     };
 
-    void update( const Eigen::Matrix<T, MeasDim, 1> meas_vec ) {
+    void update( const Eigen::Matrix<value_t, MeasDim, 1> meas_vec ) {
       // Compute residual between prediction and measurement
       // y = z - Hx'
       residual = meas_vec - meas_mtx * pre_state;
@@ -102,20 +117,20 @@ namespace cpppc
     };
 
   public:
-    Eigen::Matrix<T, StateDim, 1>           pre_state;
-    Eigen::Matrix<T, StateDim, 1>           post_state;
+    Eigen::Matrix<value_t, StateDim, 1>           pre_state;
+    Eigen::Matrix<value_t, StateDim, 1>           post_state;
 
   private:
-    Eigen::Matrix<T, StateDim, StateDim>    transition_mtx;
-    Eigen::Matrix<T, StateDim, StateDim>    pre_process_cov;
-    Eigen::Matrix<T, StateDim, StateDim>    post_process_cov;
-    Eigen::Matrix<T, StateDim, StateDim>    process_noise;
-    Eigen::Matrix<T, StateDim, CtlDim>      ctl_mtx;
+    Eigen::Matrix<value_t, StateDim, StateDim>    transition_mtx;
+    Eigen::Matrix<value_t, StateDim, StateDim>    pre_process_cov;
+    Eigen::Matrix<value_t, StateDim, StateDim>    post_process_cov;
+    Eigen::Matrix<value_t, StateDim, StateDim>    process_noise;
+    Eigen::Matrix<value_t, StateDim, CtlDim>      ctl_mtx;
 
-    Eigen::Matrix<T, MeasDim, StateDim>     meas_mtx;
-    Eigen::Matrix<T, MeasDim, MeasDim>      meas_noise;
-    Eigen::Matrix<T, StateDim, MeasDim>     gain;
-    Eigen::Matrix<T, MeasDim, 1>            residual;
+    Eigen::Matrix<value_t, MeasDim, StateDim>     meas_mtx;
+    Eigen::Matrix<value_t, MeasDim, MeasDim>      meas_noise;
+    Eigen::Matrix<value_t, StateDim, MeasDim>     gain;
+    Eigen::Matrix<value_t, MeasDim, 1>            residual;
 
   };
 
